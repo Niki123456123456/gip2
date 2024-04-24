bfb9ab8469abd44098dfd71cfc194b1d09a3f80f
Niki123456123456 <85129772+Niki123456123456@users.noreply.github.com>
2024-04-24 13:41:01 +0200

    Update main.cpp

--- sportclub/main.cpp
+++ sportclub/main.cpp
@@ -11,6 +11,19 @@ int main()
 
     TeamStatistics s = TeamStatistics(abbreviation);
 
+    int w, h;
+    while (cin >> w >> h) {
+        s.PushGame(w, h);
+    }
+
     //cout << s.Abbreviation();
     s.WriteToCout();
 }
+/*
+WH
+0 0
+0 1
+3 1
+
+WH 3 4 3:2 +1
+*/
